6e632db2c12e4a94aa124455fc10c69e772bc76e
Christoph Buelter <c.buelter@arcor.de>
2017-09-29 21:32:51 +0200

    Add script to run a testserver.

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def client():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
---
+++ runserver.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+FLASK_APP=app.py ~/.virtualenvs/steamwhat/bin/flask run --with-threads --host=0.0.0.0 --port=5000
